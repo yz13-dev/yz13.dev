2271ae4c3b172b13a179727500ea24c9878f89eb
yz13-dev <yztheceo@yandex.ru>
2026-03-10 10:28:06 +0500

    update block-size in github-graph

--- components/github-graph.tsx
+++ components/github-graph.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import {
   Activity,
   ContributionGraph,
@@ -6,21 +6,19 @@ import {
   ContributionGraphCalendar,
   ContributionGraphFooter,
   ContributionGraphLegend,
-  ContributionGraphTotalCount
+  ContributionGraphTotalCount,
 } from "@/components/kibo-ui/contribution-graph";
 import { isFuture } from "date-fns";
 import { Skeleton } from "./ui/skeleton";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
-
 export default function GithubGraph({ data = [] }: { data?: Activity[] }) {
-
   return (
-    <ContributionGraph data={data} weekStart={1}>
+    <ContributionGraph data={data} weekStart={1} blockSize={16} blockMargin={4}>
       <ContributionGraphCalendar>
         {({ activity, dayIndex, weekIndex }) => {
-          const activityDate = new Date(activity.date)
-          if (isFuture(activityDate)) return null
+          const activityDate = new Date(activity.date);
+          if (isFuture(activityDate)) return null;
           return (
             <Tooltip>
               <TooltipTrigger render={<g />}>
@@ -36,7 +34,7 @@ export default function GithubGraph({ data = [] }: { data?: Activity[] }) {
                 <p>{activity.count} активностей</p>
               </TooltipContent>
             </Tooltip>
-          )
+          );
         }}
       </ContributionGraphCalendar>
       <ContributionGraphFooter>
@@ -44,26 +42,26 @@ export default function GithubGraph({ data = [] }: { data?: Activity[] }) {
         <ContributionGraphLegend />
       </ContributionGraphFooter>
     </ContributionGraph>
-  )
+  );
 }
 
 export const GithubGraphSkeleton = () => {
   return (
     <div className="w-full flex flex-col gap-2 h-[160px] shrink-0">
-      <div className="flex items-center gap-10 justify-end">
-        <Skeleton className="w-12 h-[18px]! shrink-0" />
-        <Skeleton className="w-12 h-[18px]! shrink-0" />
-        <Skeleton className="w-12 h-[18px]! shrink-0" />
-        <Skeleton className="w-12 h-[18px]! shrink-0" />
-        <Skeleton className="w-12 h-[18px]! shrink-0" />
-        <Skeleton className="w-12 h-[18px]! shrink-0" />
-        <Skeleton className="w-12 h-[18px]! shrink-0" />
+      <div className="flex items-center *:h-5 gap-10 justify-end">
+        <Skeleton className="w-12 shrink-0" />
+        <Skeleton className="w-12 shrink-0" />
+        <Skeleton className="w-12 shrink-0" />
+        <Skeleton className="w-12 shrink-0" />
+        <Skeleton className="w-12 shrink-0" />
+        <Skeleton className="w-12 shrink-0" />
+        <Skeleton className="w-12 shrink-0" />
       </div>
-      <Skeleton className="w-full h-[110px]! shrink-0" />
+      <Skeleton className="w-full h-[130px]! shrink-0" />
       <div className="flex items-center gap-2 justify-between">
         <Skeleton className="w-1/3 h-[21px]! shrink-0" />
         <Skeleton className="w-1/3 h-[21px]! shrink-0" />
       </div>
     </div>
-  )
-}
+  );
+};
